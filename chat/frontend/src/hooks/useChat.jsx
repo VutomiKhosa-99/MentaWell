bb86ac19fa51c964b72c8a27c74e36016354b04b
VutomiKhosa-99 <khosavutomi99@gmail.com>
2023-11-14 18:57:44 +0200

    Debugging with console.log

--- chat/frontend/src/hooks/useChat.jsx
+++ chat/frontend/src/hooks/useChat.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-const backendUrl = "https://mentawell-api.onrender.com" 
+const backendUrl = "https://mentawell-api.onrender.com";
 // const backendUrl = "http://localhost:3000";
 
 const ChatContext = createContext();
