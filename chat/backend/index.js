5a9f3ae7635761b0a21fd73f6a49d788047afaf8
VutomiKhosa-99 <khosavutomi99@gmail.com>
2023-11-14 12:49:03 +0200

    Console log the messages

--- chat/backend/index.js
+++ chat/backend/index.js
@@ -150,6 +150,8 @@ app.post("/chat", async (req, res) => {
   if (messages.messages) {
     messages = messages.messages; // ChatGPT is not 100% reliable, sometimes it directly returns an array and sometimes a JSON object with a messages property
   }
+  console.log(messages)
+  return
   for (let i = 0; i < messages.length; i++) {
     const message = messages[i];
     // generate audio file
