f9e2d49ef1715e926a03674b41af11c1b3b9e385
Mpelemane <sea6580@gmail.com>
2023-11-18 12:56:49 +0200

    fixed commits

--- src/Client/src/app/app.module.ts
+++ src/Client/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
@@ -9,9 +8,7 @@ import { LoginComponent } from './pages/auth/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { ChartModule } from 'angular-highcharts';
-import { FormsModule } from "@angular/forms";
 import { PickerModule } from "@ctrl/ngx-emoji-mart";
 
 @NgModule({
@@ -28,14 +25,9 @@ import { PickerModule } from "@ctrl/ngx-emoji-mart";
     PickerModule,
     FormsModule,
     CommonModule,
-    AppRoutingModule,
-<<<<<<< HEAD
-    HttpClientModule,
-    FormsModule
-=======
     HttpClientModule
 
->>>>>>> 82b290fa92f26e2dde4955ba40664616e385922a
+
   ],
   providers: [],
   bootstrap: [AppComponent]
